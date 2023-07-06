910ea077a47464e237b78365d62cc93a3756166a
LuisTabile <luistabile@github.com>
2023-07-06 16:26:59 -0300

    :sparkless: feat: now gets the actual price in txt

--- finance.py
+++ finance.py
@@ -2,28 +2,40 @@ import yfinance as yf
 from datetime import datetime, date
 
 def obter_dados_ticker(ticker, dt_inicio, dt_fim):
-    # Convertendo a data para o formato desejado
     data_inicio = datetime.strptime(dt_inicio, '%d/%m/%Y')
     data_fim = datetime.strptime(dt_fim, '%d/%m/%Y')
 
-    # Convertendo a data para o formato desejado
     data_inicio_str = data_inicio.strftime('%Y-%m-%d')
     data_fim_str = data_fim.strftime('%Y-%m-%d')
 
-    # Obter dados históricos do ticker
-    dados = yf.download(ticker, start=data_inicio_str, end=data_fim_str, progress=False)
+    try:
+        dados = yf.download(ticker, start=data_inicio_str, end=data_fim_str, progress=False)
+    except Exception as e:
+        print(f"Erro ao obter dados do ticker: {ticker}. Erro: {str(e)}")
+        return 0.0
 
     valores_dolar = []
-
-    # Processar valores diários
     for index, row in dados.iterrows():
         valor_dolar = row['Close']
         valores_dolar.append(valor_dolar)
 
-    # Calculando o valor médio em dólar
-    valor_medio = sum(valores_dolar) / len(valores_dolar)
+    valor_medio = sum(valores_dolar) / len(valores_dolar) if valores_dolar else 0.0
     
     return valor_medio
+
+def obter_valor_atual(ticker):
+    try:
+        ticker_yf = yf.Ticker(ticker)
+        ticker_history = ticker_yf.history(period='1d')
+        if not ticker_history.empty:
+            regular_market_price = ticker_history['Close'][0]
+        else:
+            regular_market_price = 0.0
+    except Exception as e:
+        print(f"Erro ao obter valor atual do ticker: {ticker}. Erro: {str(e)}")
+        return 0.0
+
+    return regular_market_price
 
 def ler_valores_arquivo(nome_arquivo):
     with open(nome_arquivo, 'r') as arquivo:
@@ -42,24 +54,19 @@ def ler_valores_arquivo(nome_arquivo):
 
 def salvar_valores_arquivo(valores_ticker, nome_arquivo):
     with open(nome_arquivo, 'w') as arquivo:
-        for ticker, valor_medio in valores_ticker:
-            linha = f"{ticker} - {valor_medio:.2f}\n"
+        for ticker, valor_medio, valor_atual in valores_ticker:
+            linha = f"{ticker} - {valor_medio:.2f} - {valor_atual:.2f}\n"
             arquivo.write(linha)
 
-# Nome do arquivo de entrada
 nome_arquivo_entrada = "tickers.txt"
-
-# Nome do arquivo de saída
 nome_arquivo_saida = "papeis.txt"
 
-# Ler os valores do arquivo de entrada
 valores_ticker = ler_valores_arquivo(nome_arquivo_entrada)
 
-# Processar os valores dos tickers
 valores_processados = []
 for ticker, dt_inicio, dt_fim in valores_ticker:
     valor_medio = obter_dados_ticker(ticker, dt_inicio, dt_fim)
-    valores_processados.append((ticker, valor_medio))
+    valor_atual = obter_valor_atual(ticker)
+    valores_processados.append((ticker, valor_medio, valor_atual))
 
-# Salvar os valores processados no arquivo de saída
 salvar_valores_arquivo(valores_processados, nome_arquivo_saida)
